7484a4a296c7af9b838808772433c1c64d585bce
Sashik Thivanka <sashikthivankaofficial@gmail.com>
2025-08-15 01:34:43 +0530

    improved the efficiency in code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,3 @@
-/*import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home", headerShown: false }} />
-      <Stack.Screen name="about" options={{ title: "About" }} />
-    </Stack>
-  );
-}
-*/
-
-
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
